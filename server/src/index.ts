66e783d26991a9d4edcf6f82da03ac7efe08a5a4
Jim Brighter <jbrighter92@gmail.com>
2024-08-11 17:20:32 -0400

    docker image

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,6 +8,14 @@ if (!Object.values<string>(AppEnv).includes(env)) {
   throw new Error('Invalid environment provided')
 }
 
+process.on('SIGINT', () => {
+  process.exit(0)
+})
+
+process.on('SIGTERM', () => {
+  process.exit(0)
+})
+
 /**
  * Launch App
  */
