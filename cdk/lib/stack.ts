ac6d8b21afc870f111b0c56a537e1baad15c2980
Jim Brighter <jbrighter92@gmail.com>
2024-08-17 10:55:55 -0400

    clean up cdk

--- cdk/lib/stack.ts
+++ cdk/lib/stack.ts
@@ -1,49 +1,31 @@
 import * as cdk from 'aws-cdk-lib';
-import {
-  AmazonLinuxCpuType,
-  AmazonLinuxEdition,
-  AmazonLinuxGeneration,
-  AmazonLinuxImage,
-  BlockDeviceVolume,
-  CfnEIP,
-  Instance,
-  InstanceClass,
-  InstanceSize,
-  InstanceType,
-  KeyPair,
-  Peer,
-  Port,
-  SecurityGroup,
-  SubnetType,
-  UserData,
-  Vpc
-} from 'aws-cdk-lib/aws-ec2';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import { Construct } from 'constructs';
 
 export class Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const vpc = new Vpc(this, 'express-toy-vpc', {
+    const vpc = new ec2.Vpc(this, 'express-toy-vpc', {
       maxAzs: 1,
       subnetConfiguration: [{
         cidrMask: 28,
         name: 'express-toy-subnet',
-        subnetType: SubnetType.PUBLIC
+        subnetType: ec2.SubnetType.PUBLIC
       }]
     })
 
-    const securityGroup = new SecurityGroup(this, 'express-toy-sg', {
+    const securityGroup = new ec2.SecurityGroup(this, 'express-toy-sg', {
       vpc,
       allowAllOutbound: true,
       securityGroupName: 'express-toy-sg'
     })
 
-    securityGroup.addIngressRule(Peer.anyIpv4(), Port.HTTP, 'ui')
-    securityGroup.addIngressRule(Peer.anyIpv4(), Port.tcp(3000), 'api')
-    securityGroup.addIngressRule(Peer.anyIpv4(), Port.SSH, 'ssh')
+    securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.HTTP, 'ui')
+    securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(3000), 'api')
+    securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.SSH, 'ssh')
 
-    const userData = UserData.forLinux()
+    const userData = ec2.UserData.forLinux()
     userData.addCommands(
       'dnf install -y docker',
       'groupadd docker',
@@ -52,50 +34,50 @@ export class Stack extends cdk.Stack {
       'systemctl start docker'
     )
 
-    const machineImage = new AmazonLinuxImage({
-      edition: AmazonLinuxEdition.STANDARD,
-      generation: AmazonLinuxGeneration.AMAZON_LINUX_2023,
-      cpuType: AmazonLinuxCpuType.ARM_64,
+    const machineImage = new ec2.AmazonLinuxImage({
+      edition: ec2.AmazonLinuxEdition.STANDARD,
+      generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2023,
+      cpuType: ec2.AmazonLinuxCpuType.ARM_64,
       userData
     })
 
-    const jenkins = new Instance(this, 'jenkins', {
+    const jenkins = new ec2.Instance(this, 'jenkins', {
       vpc,
       securityGroup,
       machineImage,
       instanceName: 'jenkins',
-      instanceType: InstanceType.of(InstanceClass.T4G, InstanceSize.LARGE),
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T4G, ec2.InstanceSize.LARGE),
       associatePublicIpAddress: true,
       blockDevices: [
         {
           deviceName: '/dev/xvda',
-          volume: BlockDeviceVolume.ebs(60)
+          volume: ec2.BlockDeviceVolume.ebs(20)
         }
       ],
-      keyPair: KeyPair.fromKeyPairName(this, 'jenkins-key-pair', 'jim-mbp-personal')
+      keyPair: ec2.KeyPair.fromKeyPairName(this, 'jenkins-key-pair', 'jim-mbp-personal')
     })
 
-    const appServer = new Instance(this, 'express-toy', {
+    const appServer = new ec2.Instance(this, 'express-toy', {
       vpc,
       securityGroup,
       machineImage,
       instanceName: 'express-toy',
-      instanceType: InstanceType.of(InstanceClass.T4G, InstanceSize.MEDIUM),
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T4G, ec2.InstanceSize.MEDIUM),
       associatePublicIpAddress: true,
       blockDevices: [
         {
           deviceName: '/dev/xvda',
-          volume: BlockDeviceVolume.ebs(20)
+          volume: ec2.BlockDeviceVolume.ebs(10)
         }
       ],
-      keyPair: KeyPair.fromKeyPairName(this, 'app-key-pair', 'jim-mbp-personal')
+      keyPair: ec2.KeyPair.fromKeyPairName(this, 'app-key-pair', 'jim-mbp-personal')
     })
 
-    const jenkinsIp = new CfnEIP(this, 'jenkins-ip', {
+    const jenkinsIp = new ec2.CfnEIP(this, 'jenkins-ip', {
       instanceId: jenkins.instanceId
     })
 
-    const appIp = new CfnEIP(this, 'app-ip', {
+    const appIp = new ec2.CfnEIP(this, 'app-ip', {
       instanceId: appServer.instanceId
     })
   }
