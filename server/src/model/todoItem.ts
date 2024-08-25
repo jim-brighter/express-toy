525b1d9f21b9c4e80911aa64eea989683545d8eb
Jim Brighter <jbrighter92@gmail.com>
2024-08-25 16:51:22 -0400

    very typey

--- server/src/model/todoItem.ts
+++ server/src/model/todoItem.ts
@@ -1,11 +1,43 @@
 export default class TodoItem {
-  id: Number | undefined
-  text: string
-  createTime: Date | undefined
+  private _id: Number | undefined
+  private _text: string
+  private _createTime: Date | undefined
 
   constructor(o: any) {
-    this.id = o.id
-    this.text = o.text
-    this.createTime = o.create_time
+    this._id = o.id
+    this._text = o.text
+    this._createTime = o.create_time
+  }
+
+  public get id(): Number | undefined {
+    return this._id
+  }
+
+  public set id(value: Number | undefined) {
+    this._id = value
+  }
+
+  public get text(): string {
+    return this._text
+  }
+
+  public set text(value: string) {
+    this._text = value
+  }
+
+  public get createTime(): Date | undefined {
+    return this._createTime
+  }
+
+  public set createTime(value: Date | undefined) {
+    this._createTime = value
+  }
+
+  public toJSON() {
+    return {
+      id: this._id,
+      text: this._text,
+      createTime: this._createTime
+    }
   }
 }
